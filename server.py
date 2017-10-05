46593f0393a7e11a208cde63da05c5cfeb7bdbd3
Arthurlpgc <arthurlpgcosta@gmail.com>
2017-10-05 19:37:33 -0300

    Chore(server.py): create Api

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask,request,jsonify
+app = Flask("Server")
+counter = {}
+counter['cntnone']=0
+counter['cnt2aprox']=0
+counter['cntgreedy']=0
+counter['cnt2greedy']=0
+counter['mxmnone']=0
+counter['mxm2aprox']=0
+counter['mxmgreedy']=0
+counter['mxm2greedy']=0
+counter['graphs']={}
+@app.route('/graph', methods=['GET', 'POST'])
+def graph():
+	if request.method == 'POST':
+		hsh=request.form
+		print(hsh)
+		try:
+			if(hsh['none']==None or hsh['none']==0):
+				return "fail"
+			if(hsh['2greedy']==None):
+				return "fail"
+			if(hsh['greedy']==None):
+				return "fail"
+			if(hsh['2aprox']==None):
+				return "fail"
+			tmp={}
+			for otim in ['none','2aprox','greedy','2greedy']:
+				tmp['mxm'+otim]=float(hsh[otim])/float(hsh['none'])
+				counter['cnt'+otim]+=int(hsh[otim])
+				if(tmp['mxm'+otim]>counter['mxm'+otim]):
+					counter['mxm'+otim]=tmp['mxm'+otim]
+					counter['graphs'][otim]=hsh['graph']
+			return "ok"
+		except:
+			return "fail"
+	else:
+		return jsonify(counter)
