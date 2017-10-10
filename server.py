a786401b2c66d666d090732512791485c06423cc
Arthurlpgc <arthurlpgcosta@gmail.com>
2017-10-10 12:17:00 -0300

    add per graph stats

--- server.py
+++ server.py
@@ -20,17 +20,12 @@ batch={}
 batch['edges']=10
 batch['nodes']=5
 batch['count']=0
-
-graphs=[]
+graphs={}
 @app.route('/graph', methods=['GET', 'POST'])
 def graph():
 	if request.method == 'POST':
 		hsh=request.json
 		try:
-			print(hsh)
-			counter['nodes']+=hsh['nodes']
-			counter['edges']+=hsh['edges']
-			counter['requests']+=1
 			if(hsh['none']==None or hsh['none']==0):
 				return "fail"
 			if(hsh['2greedy']==None):
@@ -39,18 +34,28 @@ def graph():
 				return "fail"
 			if(hsh['2aprox']==None):
 				return "fail"
+			counter['nodes']+=hsh['nodes']
+			counter['edges']+=hsh['edges']
+			counter['requests']+=1
 			tmp={}
+			if hsh['nodes'] not in graphs.keys():
+				graphs[hsh['nodes']]={}
+			if hsh['edges'] not in graphs[hsh['nodes']].keys():
+				graphs[hsh['nodes']][hsh['edges']]={'mxmnone':0,'mxm2aprox':0,'mxmgreedy':0,'mxm2greedy':0,'cntnone':0,'cnt2aprox':0,'cntgreedy':0,'cnt2greedy':0}
 			for otim in ['none','2aprox','greedy','2greedy']:
 				tmp['mxm'+otim]=float(hsh[otim])/float(hsh['none'])
 				counter['cnt'+otim]+=int(hsh[otim])
+				graphs[hsh['nodes']][hsh['edges']]['cnt'+otim]+=int(hsh[otim])
 				if(tmp['mxm'+otim]>counter['mxm'+otim]):
 					counter['mxm'+otim]=tmp['mxm'+otim]
 					counter['graphs'][otim]=hsh['graph']
+				if(tmp['mxm'+otim]>graphs[hsh['nodes']][hsh['edges']]['mxm'+otim]):
+					graphs[hsh['nodes']][hsh['edges']]['mxm'+otim]=tmp['mxm'+otim]
 			return "ok"
 		except:
 			return "fail"
 	else:
-		return jsonify({'stats': counter,'batch': batch})
+		return jsonify({'counter': counter,'batch': batch})
 
 def magic(nodes,index):
 	if(index<5):
