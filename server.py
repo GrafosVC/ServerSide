23ea1ee4ecc1d24b94c15642266a3cdf13b67ad0
Arthurlpgc <arthurlpgcosta@gmail.com>
2017-10-12 01:46:11 -0300

    refactor

--- server.py
+++ server.py
@@ -8,6 +8,10 @@ counter['cntnone']=0
 counter['cnt2aprox']=0
 counter['cntgreedy']=0
 counter['cnt2greedy']=0
+counter['sqrnone']=0
+counter['sqr2aprox']=0
+counter['sqrgreedy']=0
+counter['sqr2greedy']=0
 counter['requests']=0
 counter['nodes']=0
 counter['edges']=0
@@ -21,73 +25,52 @@ batch['edges']=10
 batch['nodes']=5
 batch['count']=0
 graphs={}
+def update_data(hsh):
+	try:
+		if(hsh['none']==None or hsh['none']==0):
+			return "fail"
+		if(hsh['2greedy']==None):
+			return "fail"
+		if(hsh['greedy']==None):
+			return "fail"
+		if(hsh['2aprox']==None):
+			return "fail"
+		counter['nodes']+=hsh['nodes']
+		counter['edges']+=hsh['edges']
+		counter['requests']+=1
+		tmp={}
+		if hsh['nodes'] not in graphs.keys():
+			graphs[hsh['nodes']]={}
+		if hsh['edges'] not in graphs[hsh['nodes']].keys():
+			graphs[hsh['nodes']][hsh['edges']]={'graph':{},'mxmnone':0,'mxm2aprox':0,'mxmgreedy':0,'mxm2greedy':0,'cntnone':0,'cnt2aprox':0,'cntgreedy':0,'cnt2greedy':0,'sqrnone':0,'sqr2aprox':0,'sqrgreedy':0,'sqr2greedy':0}
+		for otim in ['none','2aprox','greedy','2greedy']:
+			tmp['mxm'+otim]=float(hsh[otim])/float(hsh['none'])
+			counter['cnt'+otim]+=int(hsh[otim])
+			counter['sqr'+otim]+=int(hsh[otim])*int(hsh[otim])
+			graphs[hsh['nodes']][hsh['edges']]['cnt'+otim]+=int(hsh[otim])
+			graphs[hsh['nodes']][hsh['edges']]['sqr'+otim]+=int(hsh[otim])*int(hsh[otim])
+			if(tmp['mxm'+otim]>counter['mxm'+otim]):
+				counter['mxm'+otim]=tmp['mxm'+otim]
+				counter['graphs'][otim]=hsh['graph']
+			if(tmp['mxm'+otim]>graphs[hsh['nodes']][hsh['edges']]['mxm'+otim]):
+				graphs[hsh['nodes']][hsh['edges']]['mxm'+otim]=tmp['mxm'+otim]
+				graphs[hsh['nodes']][hsh['edges']]['graph']=hsh['graph']
+	except:
+		return "fail"
+	return "ok"
+
 @app.route('/graph', methods=['GET', 'POST'])
 def graph():
 	if request.method == 'POST':
 		hshs=request.json
 		if 'graphs' in hshs.keys():
 			for hsh in hshs['graphs']:
-				try:
-					if(hsh['none']==None or hsh['none']==0):
-						return "fail"
-					if(hsh['2greedy']==None):
-						return "fail"
-					if(hsh['greedy']==None):
-						return "fail"
-					if(hsh['2aprox']==None):
-						return "fail"
-					counter['nodes']+=hsh['nodes']
-					counter['edges']+=hsh['edges']
-					counter['requests']+=1
-					tmp={}
-					if hsh['nodes'] not in graphs.keys():
-						graphs[hsh['nodes']]={}
-					if hsh['edges'] not in graphs[hsh['nodes']].keys():
-						graphs[hsh['nodes']][hsh['edges']]={'graph':{},'mxmnone':0,'mxm2aprox':0,'mxmgreedy':0,'mxm2greedy':0,'cntnone':0,'cnt2aprox':0,'cntgreedy':0,'cnt2greedy':0}
-					for otim in ['none','2aprox','greedy','2greedy']:
-						tmp['mxm'+otim]=float(hsh[otim])/float(hsh['none'])
-						counter['cnt'+otim]+=int(hsh[otim])
-						graphs[hsh['nodes']][hsh['edges']]['cnt'+otim]+=int(hsh[otim])
-						if(tmp['mxm'+otim]>counter['mxm'+otim]):
-							counter['mxm'+otim]=tmp['mxm'+otim]
-							counter['graphs'][otim]=hsh['graph']
-						if(tmp['mxm'+otim]>graphs[hsh['nodes']][hsh['edges']]['mxm'+otim]):
-							graphs[hsh['nodes']][hsh['edges']]['mxm'+otim]=tmp['mxm'+otim]
-							graphs[hsh['nodes']][hsh['edges']]['graph']=hsh['graph']
-				except:
-					return "fail"
+				ret=update_data(hsh)
+				if(ret!="ok"):
+					return ret
+			return "ok"
 		else:
-			hsh=hshs
-			try:
-				if(hsh['none']==None or hsh['none']==0):
-					return "fail"
-				if(hsh['2greedy']==None):
-					return "fail"
-				if(hsh['greedy']==None):
-					return "fail"
-				if(hsh['2aprox']==None):
-					return "fail"
-				counter['nodes']+=hsh['nodes']
-				counter['edges']+=hsh['edges']
-				counter['requests']+=1
-				tmp={}
-				if hsh['nodes'] not in graphs.keys():
-					graphs[hsh['nodes']]={}
-				if hsh['edges'] not in graphs[hsh['nodes']].keys():
-					graphs[hsh['nodes']][hsh['edges']]={'graph':{},'mxmnone':0,'mxm2aprox':0,'mxmgreedy':0,'mxm2greedy':0,'cntnone':0,'cnt2aprox':0,'cntgreedy':0,'cnt2greedy':0}
-				for otim in ['none','2aprox','greedy','2greedy']:
-					tmp['mxm'+otim]=float(hsh[otim])/float(hsh['none'])
-					counter['cnt'+otim]+=int(hsh[otim])
-					graphs[hsh['nodes']][hsh['edges']]['cnt'+otim]+=int(hsh[otim])
-					if(tmp['mxm'+otim]>counter['mxm'+otim]):
-						counter['mxm'+otim]=tmp['mxm'+otim]
-						counter['graphs'][otim]=hsh['graph']
-					if(tmp['mxm'+otim]>graphs[hsh['nodes']][hsh['edges']]['mxm'+otim]):
-						graphs[hsh['nodes']][hsh['edges']]['mxm'+otim]=tmp['mxm'+otim]
-						graphs[hsh['nodes']][hsh['edges']]['graph']=hsh['graph']
-			except:
-				return "fail"
-		return "ok"
+			return update_data(hshs)
 	else:
 		return jsonify({'counter': counter,'batch': batch, 'graphs':graphs})
 
